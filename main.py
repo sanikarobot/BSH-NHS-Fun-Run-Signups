628523742bd99742751dcac5512d867182eb873e
AaronGoyal <166578142+AaronGoyal@users.noreply.github.com>
2024-06-20 07:50:07 -0700

    Added in page functions to the main program file.

---
+++ main.py
@@ -0,0 +1,74 @@
+import tkinter as tk
+from time import sleep
+import threading
+import pickle
+
+
+def menu():
+    """
+    Main menu. Displays buttons which lead to different pages, as well as an option to go to a help page.
+    :return:
+    """
+
+
+def help_page():
+    """
+    Gives an overview of the program (through text?).
+    :return:
+    """
+
+
+def import_spreadsheet():
+    """
+    Reads in a .csv file into the program. This needs to be able to create new volunteering/tutoring logs depending on
+    the passed parameters and then return to main menu.
+    :return:
+    """
+
+
+def export_information():
+    """
+    Exports a file containing information about each student's volunteering/tutoring hours as well as their club status.
+    :return:
+    """
+
+
+def find_person():
+    """
+    Brings up a search function that will allow for searching of various club members. This doesn't search for
+    individual logs.
+    :return:
+    """
+
+
+def manage_person():
+    """
+    Displays information about a member's club status, volunteer/tutoring status, and ***any other information
+    necessary***. Also provides buttons which allow for management of the member.
+    :return:
+    """
+
+
+def manage_club_standing():
+    """
+    Provides options to add a flag to a member denoting their current status within the club. This is where people can
+    be manually changed to be in good standing, poor standing, tenuous standing, or eligible for cords.
+    :return:
+    """
+
+
+def edit_person_information():
+    """
+    Allows changes to personal information within the system. This can change the displayed email address, name, or
+    grade. This does not change information inside of the spreadsheet, rather just what is displayed within the program.
+    :return:
+    """
+
+
+def edit_entries():
+    """
+    Allows for changes to entries. This can change the contents of the spreadsheet, or it can create a new spreadsheet
+    with all the changes logged within it.
+    :return:
+    """
+
