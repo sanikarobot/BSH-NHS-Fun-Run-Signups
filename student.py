81d03646511e123a94f74a1fe7333de5dced5f8a
AaronGoyal <166578142+AaronGoyal@users.noreply.github.com>
2024-06-20 20:57:22 -0700

    Basic Student class created for testing purposes, this can be overwritten. Contains student name and student id.

--- student.py
+++ student.py
@@ -1,1 +1,26 @@
 
+
+class Student:
+
+    next_student_id = 0
+
+    def __init__(self, name):
+        self.name = name
+        self.student_id = Student.next_student_id
+        Student.next_student_id = Student.next_student_id + 1
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+        
+    @property
+    def student_id(self):
+        return self._student_id
+
+    @student_id.setter
+    def student_id(self, student_id):
+        self._student_id = student_id
