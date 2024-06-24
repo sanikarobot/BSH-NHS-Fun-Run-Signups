4d5fdd449fa325f2a730b224261f04109ddc6b82
AaronGoyal <166578142+AaronGoyal@users.noreply.github.com>
2024-06-23 21:06:24 -0700

    Custom Exception Class module allows for better handling for exceptions created by the programmer (eg. if you want to give a specific exception created by the program if a parameter value is not set correctly)

---
+++ customException.py
@@ -0,0 +1,8 @@
+class CustomException(Exception):
+    """
+    Built to handle custom exceptions better. This helps differentiate exceptions between ones that we create and ones
+    that Python throws in order to prevent us from making major mistakes.
+    This class inherits from Exception and provides the same functionality.
+    """
+    def __init__(self, message):
+        super.__init__(message)
