6830d63960629d7c7126a0026c983d3380bab3d8
AaronGoyal <166578142+AaronGoyal@users.noreply.github.com>
2024-06-27 12:20:52 -0700

    Reformatted the file to offer different built-in functions for MDY setters/getters in a pythonic way.

--- volunteer.py
+++ volunteer.py
@@ -1,16 +1,20 @@
 from threading import Thread
+import customException
 
 class Volunteer():
-    def __init__(self, title: str, email: str, date: str, time: float, location: str, notes: str) -> None:
+    def __init__(self, title: str, email: str, date: str, time: float, location: str, notes: str = "") -> None:
+        # Copypaste: volunteer.Volunteer(title, email, date, time, location)
         self.title = title
         self.email = email
         self.date = date
+        self._dateInts = []
+        self.analyzeDate()
+        self.month = self._dateInts[0]
+        self.day = self._dateInts[1]
+        self.year = self._dateInts[2]
         self.time = time
         self.location = location
-        self.month = ""
-        self.day = ""
-        self.year = ""
-        self.dateInts = []
+
 
     @property
     def title(self)-> str:
@@ -59,49 +63,98 @@ class Volunteer():
     @notes.setter
     def notes(self, notes)-> None:
         self._notes = notes
-        
-    # We should also add in a notes section as 
-    
-    def analyizeDate(self):
+
+    @property
+    def month(self)-> str:
+        return self._month
+
+    @month.setter
+    def month(self, value):
+        self._month = ""
+        if value == 1:
+            self._month= "January"
+        elif value == 2:
+            self._month= "February"
+        elif value == 3:
+            self._month= "March"
+        elif value == 4:
+            self._month= "April"
+        elif value == 5:
+            self._month= "May"
+        elif value == 6:
+            self._month= "June"
+        elif value == 7:
+            self._month= "July"
+        elif value == 8:
+            self._month= "August"
+        elif value == 9:
+            self._month= "September"
+        elif value == 10:
+            self._month= "October"
+        elif value == 11:
+            self._month= "November"
+        elif value == 12:
+            self._month= "December"
+        else:
+            raise customException.CustomException("Value provided for month out of bounds (1-12 required)")
+
+    @property
+    def day(self)-> str:
+        return self._day
+
+    @day.setter
+    def day(self, value) -> None:
+        self._day = value
+
+    @property
+    def year(self)-> str:
+        return self._year
+
+    @year.setter
+    def year(self, value) -> None:
+        self._year = value
+
+    def analyzeDate(self):
         if "/" in self.date:
-            self.dateInts = self.date.split("/")
+            self._dateInts = self.date.split("/")
         elif "-" in self.date:
-            self.date.split('-')
-        return self.dateInts
+            self._dateInts = self.date.split('-')
+        return self._dateInts
     
     def getMonth(self)-> str:
-        numMonth = self.dateInts[0]
-        self.month = ""
+        numMonth = self._dateInts[0]
+        self.month = self._dateInts[0]
         if numMonth == 1:
-            self.month = "January"
+            return "January"
         elif numMonth == 2:
-            self.month = "February"
+            return "February"
         elif numMonth == 3:
-            self.month = "March"
+            return "March"
         elif numMonth == 4:
-            self.month = "April"
+            return "April"
         elif numMonth == 5:
-            self.month = "May"
+            return "May"
         elif numMonth == 6:
-            self.month = "June"
+            return "June"
         elif numMonth == 7:
-            self.month = "July"
+            return "July"
         elif numMonth == 8:
-            self.month = "August"
+            return "August"
         elif numMonth == 9:
-            self.month = "September"
+            return "September"
         elif numMonth == 10:
-            self.month = "October"
+            return "October"
         elif numMonth == 11:
-            self.month = "Novemeber"
+            return "November"
         elif numMonth == 12:
-            self.month = "December"
-        return self.month
+            return "December"
+        else:
+            raise customException.CustomException("Value provided for month out of bounds (1-12 required)")
     
     def getDay(self)-> str:
-        self.day = self.dateInts[1]
+        self.day = self._dateInts[1]
         return self.day
     
     def getYear(self)-> str:
-        self.year = self.dateInts[2]
+        self.year = self._dateInts[2]
         return self.year
